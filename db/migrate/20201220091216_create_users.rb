e486749f698f0fda0b2c91ac599d69e1873c2d1f
your-name <your-email>
2020-12-21 17:02:14 +0900

    itemsコントローラーitems.rbのmigrationファイル作成
    items.rbのmigratipnファイルに
          t.string :name
          t.string :text
          t.text :image
    の追加

--- db/migrate/20201220091216_create_users.rb
+++ db/migrate/20201220091216_create_users.rb
@@ -4,14 +4,12 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name,      null:false
       t.string :family_name,     null:false
       t.string :first_name_kana, null:false
-      t.string :family_name,     null:false
+      t.string :family_name_kana,     null:false
       t.string :nickname,        null:false  
-      t.string :email,           null:false
       t.string :password,        null:false
       t.integer :age,            null:false
       t.date :birth_day,         null:false
       t.timestamps               null:false
-
     end
   end
 end
