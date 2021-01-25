6d529f0461bacee8dfb76b9d23ba2a2dc815168d
ryuseikorogi <ryuseikorogi@gmail.com>
2021-01-25 21:32:21 +0900

    ハンバーグボタンの追加と写真のヒーローイメージの変更

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,15 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+//ハンバーガーボタンの記述
+$(document).ready(function() {
+  $(".menu-trigger").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().toggleClass("onanimation");
+    $('ul li').hide();
+    $('ul li').each(function(i) {
+      $(this).delay(80 * i).fadeIn(500);
+    });
+  });
+});
