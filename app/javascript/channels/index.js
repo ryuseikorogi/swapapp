6c46eaa93cf4af9df4704b8dfcc4d77c26689429
ryuseikorogi <ryuseikorogi@gmail.com>
2021-01-28 19:45:30 +0900

    ハンバーガーメニューの編集

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,14 +4,13 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-//ハンバーガーボタンの記述
-$(document).ready(function() {
-  $(".menu-trigger").click(function () {
-    $(this).toggleClass("active");
-    $(this).next().toggleClass("onanimation");
-    $('ul li').hide();
-    $('ul li').each(function(i) {
-      $(this).delay(80 * i).fadeIn(500);
-    });
-  });
-});
+//ハンバーガーボタンの動き
+function hamburger() {
+  document.getElementById('line1').classList.toggle('line_1');
+  document.getElementById('line2').classList.toggle('line_2');
+  document.getElementById('line3').classList.toggle('line_3');
+  document.getElementById('nav').classList.toggle('in');
+}
+document.getElementById('hamburger').addEventListener('click' , function () {
+  hamburger();
+} );
