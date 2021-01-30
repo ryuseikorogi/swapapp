2e2fea1e4cc683f8b7a17fcebd9ea977a1d2a7da
ryuseikorogi <ryuseikorogi@gmail.com>
2021-01-30 23:34:22 +0900

    javascriptの修正とviewsの調整

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,14 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-//ハンバーガーボタンの動き
-function hamburger() {
-  document.getElementById('line1').classList.toggle('line_1');
-  document.getElementById('line2').classList.toggle('line_2');
-  document.getElementById('line3').classList.toggle('line_3');
-  document.getElementById('nav').classList.toggle('in');
-}
-document.getElementById('hamburger').addEventListener('click' , function () {
-  hamburger();
-} );
