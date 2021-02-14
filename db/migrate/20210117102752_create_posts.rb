0dda6dd48ee03b2e3a80ca651bc827aa89899acf
ryuseikorogi <ryuseikorogi@gmail.com>
2021-02-14 21:57:37 +0900

    activestorageの画像を表示できる機能の実装

--- db/migrate/20210117102752_create_posts.rb
+++ db/migrate/20210117102752_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.references :user, foreign_key: true
+      t.references :user, 
+      foreign_key: true
         t.string :post_name,                null: false
         t.string :post_discription,              null: false
         t.string :prefecture_id, null: false
